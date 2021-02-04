b8452840665298e0a8ee531c16ac9c67b6e939e3
itaweb <admod32@gmail.com>
2021-02-05 00:36:07 +0300

    Fin code

---
+++ index.py
@@ -0,0 +1,26 @@
+﻿import sys
+import random
+print('Добро пожаловать в генератор забавных имён!\n')
+print('Например, вот забавное имя для твоего друга: \n\n')
+
+first = ('Чайник', 'Кипятильник', 'Рубильник',
+         'Пылесос', 'Пуховик', 'Клоун', 'Пёс', 'Пудель')
+
+last = ('Мохнорылый', 'Толстожопый', 'Не приятный', 'Плохопахнущий', 'Кучерявый',
+        'Отмороженный', 'Облезлый', 'Завистливый')
+
+while True:
+
+    firstName = random.choice(first)
+    lastName = random.choice(last)
+
+    print('\n\n')
+
+    print('{} {}'.format(firstName, lastName), file=sys.stderr)
+
+    try_again = input(
+        '\n\nХотите попробовать ещё? (Нажмите Enter если ДА, и нажмите - n - если хотите выйти...)\n')
+    if try_again.lower() == 'n':
+        break
+
+input("\пНажмите Enter для завершения работы.")
